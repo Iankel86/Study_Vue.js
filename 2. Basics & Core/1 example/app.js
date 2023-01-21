02bf7086ec1a8a6ad286e4491e923b53bc39f2b2
Ян Шадрин <Yankelll86@mail.ru>
2023-01-21 15:06:52 +0400

    test comint

---
+++ 2. Basics & Core/1 example/app.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoal: 'ууухххх'
+        };
+    }
+});
+
+app.mount('#user-goal');    // метод монтирования этого приложения, при помощи #user-goa подключаем  
