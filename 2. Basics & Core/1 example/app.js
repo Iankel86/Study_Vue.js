98faf0e1af0e8bcead41cdfd658af60caa5e9aa6
Ян Шадрин <Yankelll86@mail.ru>
2023-01-22 15:11:45 +0400

    fix: created the main README.md throughout the course-5

--- 2. Basics & Core/1 example/app.js
+++ 2. Basics & Core/1 example/app.js
@@ -1,8 +1,24 @@
 const app = Vue.createApp({
-    data() {
+    data() {        // сами данные были функцией методов 
         return {
-            courseGoal: 'ууухххх'
+            courseGoalA: 'Закончить курс',
+            courseGoalB: 'Мастерский выучить Vue',
+            // courseGoalB: '<h2>Мастерский выучить Vue</h2>', - это по примеру к v-html
+            vueLink: 'https://vuejs.org/'
         };
+    },
+    // опция методов - позволяют определять функции, которые должны выполняться, когда что то происходит, например нажатие кнопки
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();     // Math.random - математическая случайность
+            if (randomNumber < 0.5) {
+                return this.courseGoalA;
+                // return 'Все верно!';
+            }   else {
+                return this.courseGoalB;
+                // return 'Ошибка!';
+            }
+        }
     }
 });
 
