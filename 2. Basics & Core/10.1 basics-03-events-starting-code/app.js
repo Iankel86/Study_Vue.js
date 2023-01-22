0ca726cc72af58a1c556b7c9f1723644897d0aaa
Ян Шадрин <Yankelll86@mail.ru>
2023-01-22 22:31:49 +0400

    feat: I did an exercise with practice No. 10 on the 2nd topic of the 10th lesson.

---
+++ 2. Basics & Core/10.1 basics-03-events-starting-code/app.js
@@ -0,0 +1,30 @@
+// document.querySelector('button').addEventListener() // так было бы сделано в ванильном JS, н опришлось бы указывать шаг за шагом все.
+
+const app = Vue.createApp({
+  data() {    // метод передачи данных
+    return {
+      counter: 20,
+      name: '',
+      confirmedName: ''
+    };
+  },
+  methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm () {
+      alert('Sabmitted!');
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+    addcounter(num) {
+      this.counter = this.counter + num;
+    },
+    addreduce(num) {
+      this.counter = this.counter - num;
+    }
+  }
+});
+
+app.mount('#events');
