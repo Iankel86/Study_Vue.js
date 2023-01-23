72f2071bdef7858c6014caa571c61c1758ea82d3
Ян Шадрин <Yankelll86@mail.ru>
2023-01-23 16:25:51 +0400

    feat: I did an exercise with practice No. 17 on the 2nd topic of the 17th lesson.

--- 2. Basics & Core/17.1 basics-05-using-the-native-event-object/app.js
+++ 2. Basics & Core/17.1 basics-05-using-the-native-event-object/app.js
@@ -2,12 +2,21 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      // fullname: '',
     };
   },
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+    // outputFullname() {
+    //   console.log('Возврат...');         //Это не очень хорошй метод, т.к. перезагружается все методы, есть более правильный, через computed:
+    //   if (this.name === '') {
+    //     return '';
+    //   }
+    //   return this.name + ' ' + 'Шадрин';
+    // },
+    setName(event) {
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -15,7 +24,40 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
+    },
+    resetInput() {
+      this.name = '';
     }
+  },
+  computed: {
+    fullname() {
+      console.log('Возврат...');
+      if (this.name === '' || this.lastName === '') {
+        return '';
+      }
+      return this.name + ' ' + this.lastName;
+    }
+  },
+  watch: {      // Наблюдатель - это более длинный код получается, чем в computed:, но если ты хочешь проверить кол-во по счетчику, то здесь наблюдатель более удобен., или таймеры, или http запросы
+    counter(value) {
+      if (value > 50) {
+        this.counter = 0;
+      }
+    }
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = this.name + ' ' + value;
+    //   }
+    // }
   }
 });
 
