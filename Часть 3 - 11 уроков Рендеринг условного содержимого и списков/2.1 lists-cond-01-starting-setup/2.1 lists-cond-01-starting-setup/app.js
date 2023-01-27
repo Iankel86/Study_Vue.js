df38faa1ee176683561fb78bf321b5cad44afb9a
Ян Шадрин <Yankelll86@mail.ru>
2023-01-27 17:07:31 +0400

    feat: I did task No. 2.1 for 3 parts

--- Часть 3 - 11 уроков Рендеринг условного содержимого и списков/2.1 lists-cond-01-starting-setup/2.1 lists-cond-01-starting-setup/app.js
+++ Часть 3 - 11 уроков Рендеринг условного содержимого и списков/2.1 lists-cond-01-starting-setup/2.1 lists-cond-01-starting-setup/app.js
@@ -1,7 +1,23 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { 
+      // хочу прописать все, что пользователь ввел в этом ввод:
+      enteredGoalValue: '',   // назвать можно было как захочешь
+      // теперь избавляемся от жестко закодированной цели:
+      goals: []
+    };
   },
+  // Добавляем методы
+  methods: {
+    // теперь в методе добавлений могу обратиться к целе
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+    },
+    removeGoal(idx) {    // если хотим что то удалить
+      this.goals.splice(idx, 1);      // splice найдет по индексу и удалит
+    }
+ 
+  }
 });
 
 app.mount('#user-goals');
